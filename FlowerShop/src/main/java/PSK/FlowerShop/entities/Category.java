1f4023e549f71fe500da57837760393e70b860ea
povilasl <povilasleka@gmail.com>
2023-04-18 20:56:28 +0300

    Create jpa entities. Add spring-boot-starter-data-jpa to pom.xml

---
+++ FlowerShop/src/main/java/PSK/FlowerShop/entities/Category.java
@@ -0,0 +1,18 @@
+package PSK.FlowerShop.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @OneToMany
+    private List<Product> products;
+    private String name;
+}

af259e5d7c5ec537c79876df83568f744acea24c
AllBill127 <90201218+AllBill127@users.noreply.github.com>
2023-04-22 01:18:04 +0300

    add Product dao layer
    add Product repository
    add Product CRUD methods to service

---
+++ FlowerShop/src/main/java/PSK/FlowerShop/service/ProductService.java
@@ -0,0 +1,52 @@
+package PSK.FlowerShop.service;
+
+import PSK.FlowerShop.entities.Category;
+import PSK.FlowerShop.entities.Product;
+import PSK.FlowerShop.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    ProductRepository productRepository;
+
+    public void createProduct(Product product) {
+        productRepository.save(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public List<Product> getProductsByCategory(Category category) {
+        return productRepository.findAllByCategory(category);
+    }
+
+    public Optional<Product> getProductById(Integer id) {
+        return productRepository.findById(id);
+    }
+
+    public void updateProduct(Product product) throws Exception {
+        if(productRepository.existsById(product.getId()))
+            productRepository.save(product);
+        else
+            throw new Exception(String.format(
+                    "Product to update with ID:{0} does not exist",
+                    product.getId()
+            ));
+    }
+
+    public void deleteProduct(Integer id) throws Exception {
+        if(productRepository.existsById(id))
+            productRepository.deleteById(id);
+        else
+            throw new Exception(String.format(
+                    "Product to delete with ID:{0} does not exist",
+                    id
+            ));
+    }
+}

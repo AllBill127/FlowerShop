72efbe5acd6f101430f4a6a5edb8a31454019f4e
Justuxs <pegasas@yahoo.com>
2023-05-26 15:28:47 +0300

    Cros. Now no need to disable web security.

--- FlowerShop/src/main/java/PSK/FlowerShop/FlowerShopApplication.java
+++ FlowerShop/src/main/java/PSK/FlowerShop/FlowerShopApplication.java
@@ -2,11 +2,11 @@ package PSK.FlowerShop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-
 public class FlowerShopApplication {
 
 	public static void main(String[] args) {
